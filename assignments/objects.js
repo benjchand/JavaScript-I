51f46f86b77848d14e9849c68b49585c811b7761
benjchand <benchand@gmail.com>
2019-02-25 16:48:51 -0800

    Worked on stretch goals.

--- assignments/objects.js
+++ assignments/objects.js
@@ -81,7 +81,7 @@ console.log(secondIntern.speak)
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
-console.log(fifthIntern.multiplyNums(4,5))
+console.log(fifthIntern.multiplyNums(14,500))
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
@@ -92,16 +92,36 @@ console.log(fifthIntern.multiplyNums(4,5))
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+const parent = {
+  "name": "Susan",
+  "age": "70",
+  "speak": "I'm Susan.",
+  "child": {
+    "name": "George",
+    "age": "50",
+    "speak": "I'm George.",
+    "grandchild":  {
+      "name": "Sam",
+      "age": "30",
+      "speak": "I'm Sam.",
+    }
+  }
+}
 
 // Log the parent object's name
+console.log(parent.name)
 
 // Log the child's age
+console.log(parent.child.age)
 
 // Log the name and age of the grandchild
+console.log(parent.child.grandchild.name, parent.child.grandchild.age)
 
 // Have the parent speak
+console.log(parent.speak)
 
-// Have the child speak
+// Have the child speak 
+console.log(parent.child.speak)
 
 // Have the grandchild speak
+console.log(parent.child.grandchild.speak)
